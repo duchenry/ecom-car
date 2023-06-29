67a62fb60502fe3388b401bcae3b733449516695
Henry <henrytech4365@gmail.com>
2023-06-29 09:30:47 +0700

    fix

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import SearchManufacturer from "./SearchManufacturer";
 
 const SearchBar = () => {
-  const [manufacturer, setManufacturer] = useState();
+  const [manufacturer, setManufacturer] = useState("");
   const handleSearch = () => {};
   return (
     <form className="searchbar" onSubmit={handleSearch}>
